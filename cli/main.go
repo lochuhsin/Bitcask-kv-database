8bcf905fb12d7086fe4a527936072572c9af73d9
albertlo <lochuhsin@gmail.com>
2022-10-25 00:07:29 +0800

    feat: temporary commit, implement all for operations(GET,POST,PATCH,DELETE) next time

--- cli/main.go
+++ cli/main.go
@@ -2,120 +2,87 @@ package main
 
 import (
 	"fmt"
-	"net"
-	"os"
-	"rebitcask/internal"
-	"strings"
+	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"sync"
 	"time"
 )
 
-// TODO: Convert using envFile
-const (
-	ServerHost = "127.0.0.1"
-	ServerPort = 6666
-	ServerType = "tcp"
-)
+//func sayhelloName(w http.ResponseWriter, r *http.Request) {
+//	r.ParseForm()
+//	fmt.Println("form", r.Form)
+//	fmt.Println("method", r.Method)
+//	fmt.Println("path", r.URL.Path)
+//	fmt.Println("scheme", r.URL.Scheme)
+//	for k, v := range r.Form {
+//		fmt.Println("key:", k)
+//		fmt.Println("val:", strings.Join(v, ""))
+//	}
+//	fmt.Fprintf(w, "Hello astaxie!") //這個寫入到 w 的是輸出到客戶端的
+//}
 
-var ConnQueue ConnectionQueue
-
-func init() {
-
-	// create connection queue as buffer
-	ConnQueue = CreateQueue()
-	// start handler
-	go connectionHandler()
+type conn struct {
+	resp http.ResponseWriter
+	req  *http.Request
+	id   string
 }
 
-/*
-this server uses tcp/ip connection
-format:
-Get:<key>
-Set:<key>,<val>
-Delete:<key>
-*/
+type connQueue struct {
+	mu         sync.Mutex
+	queryQueue []conn
+}
+
+type queryHandler struct{}
+
+var Queue = new(connQueue)
+
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	_ = r.ParseForm()
+	fmt.Println(r.URL)
+	fmt.Println(r.Method)
+	fmt.Println(r.URL.Scheme)
+	fmt.Println(r.URL.RawQuery)
+	fmt.Println("form", r.Form)
+	fmt.Fprintf(w, "Welcome to rebitcask") // send message back
+}
+
+// / direction of queue   left is output, right is input (opposite from python)
+func EventLoop() {
+	for {
+		if len(Queue.queryQueue) > 0 {
+			//_ = Queue.queryQueue[0]
+			//Queue.queryQueue = Queue.queryQueue[1:]
+			// TODO: Write GET, POST, PATCH, DELETE ..... etc over here
+		}
+
+		time.Sleep(3 * time.Second)
+		fmt.Println("I'm still running, the length of Queue", len(Queue.queryQueue))
+	}
+}
+
+func (query *queryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	connection := conn{
+		resp: w,
+		req:  r,
+		id:   uuid.New().String(),
+	}
+	Queue.mu.Lock()
+	Queue.queryQueue = append(Queue.queryQueue, connection)
+	Queue.mu.Unlock()
+
+	fmt.Println("Currently have ", len(Queue.queryQueue), "connection waiting")
+}
 
 func main() {
-	fmt.Println("Server starts to run ...")
+	fmt.Println("Start server")
+	go EventLoop()
 
-	server, err := net.Listen(ServerType, fmt.Sprintf("%v:%v", ServerHost, ServerPort))
+	http.HandleFunc("/", helloServer)
+	http.Handle("/query", new(queryHandler))
+	err := http.ListenAndServe(":6666", nil) //設定監聽的埠
 	if err != nil {
-		fmt.Println("Starting server error, shutting down", err.Error())
-		os.Exit(1)
-	}
-
-	for {
-		conn, err := server.Accept()
-		if err != nil {
-			fmt.Println("Error accepting: ", err.Error())
-			continue
-		}
-		// TODO: convert to goroutine
-		go insertQueueHandler(conn)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
-
-func insertQueueHandler(conn net.Conn) {
-	ConnQueue.Enqueue(conn)
-}
-
-func connectionHandler() {
-	for {
-		if qSize := ConnQueue.GetSize(); qSize > 0 {
-			conn, status := ConnQueue.Dequeue()
-			if status != true {
-				// figure out handling this
-			}
-
-			buffer := make([]byte, 1024)
-			_, _ = conn.Read(buffer)
-
-			operations := strings.Split(string(buffer), ":")
-			if len(operations) != 2 {
-				conn.Write([]byte("400"))
-			} else {
-				method, values := operations[0], operations[1]
-				fmt.Println(method, values)
-				if method == "GET" {
-					outputs, status := internal.Get(values)
-					fmt.Println("Get status: ", status, method, values)
-					if !status {
-						conn.Write([]byte("400"))
-					} else {
-						conn.Write([]byte(outputs))
-					}
-				}
-				if method == "DELETE" {
-					err := internal.Delete(values)
-					if err != nil {
-						conn.Write([]byte("400"))
-					} else {
-						conn.Write([]byte("200"))
-					}
-				}
-				if method == "SET" {
-					val := strings.Split(values, ",")
-					if len(val) != 2 {
-						conn.Write([]byte("400"))
-						continue
-					}
-					err := internal.Set(val[0], val[1])
-					if err != nil {
-						conn.Write([]byte("400"))
-					} else {
-						conn.Write([]byte("200"))
-					}
-				}
-			}
-			conn.Close()
-		}
-		time.Sleep(1 * time.Second)
-	}
-}
-
-// this is none stopping method of keep reading file
-//scanner := bufio.NewScanner(conn)
-//for scanner.Scan() {
-//	ln := scanner.Text()
-//	fmt.Println(ln)
-//}
-//conn.Close()
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rebitcask
 
 go 1.19
 
-require github.com/joho/godotenv v1.4.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
+)

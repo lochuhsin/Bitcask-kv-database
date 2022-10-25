be7464e8c60689194675a317e91f425e1c44acf8
albertlo <lochuhsin@gmail.com>
2022-10-25 22:45:24 +0800

    feat: implement http based server

--- cli/main.go
+++ cli/main.go
@@ -2,40 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/google/uuid"
 	"log"
 	"net/http"
-	"sync"
-	"time"
+	"rebitcask/cli/Handler"
 )
-
-//func sayhelloName(w http.ResponseWriter, r *http.Request) {
-//	r.ParseForm()
-//	fmt.Println("form", r.Form)
-//	fmt.Println("method", r.Method)
-//	fmt.Println("path", r.URL.Path)
-//	fmt.Println("scheme", r.URL.Scheme)
-//	for k, v := range r.Form {
-//		fmt.Println("key:", k)
-//		fmt.Println("val:", strings.Join(v, ""))
-//	}
-//	fmt.Fprintf(w, "Hello astaxie!") //這個寫入到 w 的是輸出到客戶端的
-//}
-
-type conn struct {
-	resp http.ResponseWriter
-	req  *http.Request
-	id   string
-}
-
-type connQueue struct {
-	mu         sync.Mutex
-	queryQueue []conn
-}
-
-type queryHandler struct{}
-
-var Queue = new(connQueue)
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
 	_ = r.ParseForm()
@@ -47,40 +17,10 @@ func helloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to rebitcask") // send message back
 }
 
-// / direction of queue   left is output, right is input (opposite from python)
-func EventLoop() {
-	for {
-		if len(Queue.queryQueue) > 0 {
-			//_ = Queue.queryQueue[0]
-			//Queue.queryQueue = Queue.queryQueue[1:]
-			// TODO: Write GET, POST, PATCH, DELETE ..... etc over here
-		}
-
-		time.Sleep(3 * time.Second)
-		fmt.Println("I'm still running, the length of Queue", len(Queue.queryQueue))
-	}
-}
-
-func (query *queryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	connection := conn{
-		resp: w,
-		req:  r,
-		id:   uuid.New().String(),
-	}
-	Queue.mu.Lock()
-	Queue.queryQueue = append(Queue.queryQueue, connection)
-	Queue.mu.Unlock()
-
-	fmt.Println("Currently have ", len(Queue.queryQueue), "connection waiting")
-}
-
 func main() {
 	fmt.Println("Start server")
-	go EventLoop()
-
 	http.HandleFunc("/", helloServer)
-	http.Handle("/query", new(queryHandler))
+	http.Handle("/query", new(handler.QueryHandler))
 	err := http.ListenAndServe(":6666", nil) //設定監聽的埠
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
--- test/rebitcask_test.go
+++ test/rebitcask_test.go
@@ -2,48 +2,11 @@ package test
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 	"rebitcask/src"
 	"testing"
 	"time"
 )
-
-var LETTERS = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-func generatePureRandomData() map[string]string {
-	rand.Seed(time.Now().UnixNano())
-	ans := make(map[string]string)
-	for i := 0; i < 100000; i++ {
-		k := make([]rune, rand.Intn(30))
-		for i := range k {
-			k[i] = LETTERS[rand.Intn(len(LETTERS))]
-		}
-		v := make([]rune, rand.Intn(30))
-		for i := range v {
-			v[i] = LETTERS[rand.Intn(len(LETTERS))]
-		}
-		ans[string(k)] = string(v)
-	}
-	return ans
-}
-
-func generateKeyDuplicateRandomData() map[string]string {
-	rand.Seed(time.Now().UnixNano())
-	ans := make(map[string]string)
-	for i := 0; i < 100000; i++ {
-		k := make([]rune, 2)
-		for i := range k {
-			k[i] = LETTERS[rand.Intn(len(LETTERS))]
-		}
-		v := make([]rune, rand.Intn(30))
-		for i := range v {
-			v[i] = LETTERS[rand.Intn(len(LETTERS))]
-		}
-		ans[string(k)] = string(v)
-	}
-	return ans
-}
 
 func TestGetSetPureRandom(t *testing.T) {
 	ans := generatePureRandomData()

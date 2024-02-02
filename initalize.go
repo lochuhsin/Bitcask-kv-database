99b461e8d31f53742a92dbcdbadb8a9b725ebfa2
lochuhsin <lochuhsin@gmail.com>
2024-02-02 08:38:31 +0800

    feat(settings): refactor startup

--- initalize.go
+++ initalize.go
@@ -1,8 +1,6 @@
 package rebitcask
 
 import (
-	"fmt"
-	"os"
 	"rebitcask/internal/memory"
 	"rebitcask/internal/scheduler"
 	"rebitcask/internal/segment"
@@ -13,15 +11,8 @@ func Init(envPaths ...string) {
 	/**
 	 * Should call this, whenever the server is up
 	 */
-
 	settings.InitConfig(envPaths...)
-	config := settings.Config
-
-	segDir := fmt.Sprintf("%s%s", config.DATA_FOLDER_PATH, settings.SEGMENT_FILE_FOLDER)
-	indexDir := fmt.Sprintf("%s%s", config.DATA_FOLDER_PATH, settings.INDEX_FILE_FOLDER)
-	os.MkdirAll(segDir, os.ModePerm)
-	os.MkdirAll(indexDir, os.ModePerm)
-
+	settings.SetUpDirectory()
 	memory.InitMemoryManager(memory.ModelType(settings.Config.MEMORY_MODEL))
 	segment.InitSegmentManager()
 	scheduler.InitScheduler()
--- internal/settings/util.go
+++ internal/settings/util.go
@@ -1,6 +1,10 @@
 package settings
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+)
 
 var cOnce sync.Once
 
@@ -20,4 +24,12 @@ func InitConfig(envPaths ...string) {
 			)
 		},
 	)
+}
+
+func SetUpDirectory() {
+
+	segDir := fmt.Sprintf("%s%s", Config.DATA_FOLDER_PATH, SEGMENT_FILE_FOLDER)
+	indexDir := fmt.Sprintf("%s%s", Config.DATA_FOLDER_PATH, INDEX_FILE_FOLDER)
+	os.MkdirAll(segDir, os.ModePerm)
+	os.MkdirAll(indexDir, os.ModePerm)
 }

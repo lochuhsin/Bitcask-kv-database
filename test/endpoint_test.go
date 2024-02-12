eebb1d8a594be59d864bfb027f986f64465a9075
lochuhsin <lochuhsin@gmail.com>
2024-02-12 14:01:02 +0800

    fix test

--- test/endpoint_test.go
+++ test/endpoint_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"os"
 	"rebitcask"
 	"rebitcask/internal/settings"
@@ -24,34 +25,32 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-// func TestStorageSet(t *testing.T) {
-// 	env := settings.ENV
-// 	dataCount := env.MemoryCountLimit*10 + 1
+func TestStorageSet(t *testing.T) {
+	dataCount := settings.Config.MEMORY_COUNT_LIMIT*10 + 1
 
-// 	keys, vals := GenerateLowDuplicateRandom(dataCount)
-// 	for i, k := range keys {
-// 		err := rebitcask.Set(k, vals[i])
-// 		if err != nil {
-// 			t.Error("Something went wrong while setting")
-// 		}
-// 	}
-// }
+	keys, vals := GenerateLowDuplicateRandom(dataCount)
+	for i, k := range keys {
+		err := rebitcask.Set(k, vals[i])
+		if err != nil {
+			t.Error("Something went wrong while setting")
+		}
+	}
+}
 
-// func TestStorageGet(t *testing.T) {
+func TestStorageGet(t *testing.T) {
 
-// 	env := settings.ENV
-// 	dataCount := env.MemoryCountLimit*10 + 1
+	dataCount := settings.Config.MEMORY_COUNT_LIMIT*10 + 1
 
-// 	keys, _ := GenerateLowDuplicateRandom(dataCount)
-// 	for _, k := range keys {
-// 		_, _ = rebitcask.Get(k)
-// 	}
-// }
+	keys, _ := GenerateLowDuplicateRandom(dataCount)
+	for _, k := range keys {
+		_, _ = rebitcask.Get(k)
+	}
+}
 
 func TestStorageDelete(t *testing.T) {
 
-	env := settings.Config
-	dataCount := env.MEMORY_COUNT_LIMIT*1 + 1
+	config := settings.Config
+	dataCount := config.MEMORY_COUNT_LIMIT*1 + 1
 	keys, _ := GenerateLowDuplicateRandom(dataCount)
 	for _, k := range keys {
 		err := rebitcask.Delete(k)
@@ -87,42 +86,40 @@ func TestStorageSetGet(t *testing.T) {
 	}
 }
 
-// func TestStorageSetDelete(t *testing.T) {
-// 	env := settings.ENV
-// 	dataCount := env.MemoryCountLimit*10 + 1
+func TestStorageSetDelete(t *testing.T) {
+	dataCount := settings.Config.MEMORY_COUNT_LIMIT*10 + 1
 
-// 	keys, vals := GenerateLowDuplicateRandom(dataCount)
-// 	for i, k := range keys {
-// 		err := rebitcask.Set(k, vals[i])
-// 		if err != nil {
-// 			t.Error("Something went wrong while setting")
-// 		}
-// 	}
+	keys, vals := GenerateLowDuplicateRandom(dataCount)
+	for i, k := range keys {
+		err := rebitcask.Set(k, vals[i])
+		if err != nil {
+			t.Error("Something went wrong while setting")
+		}
+	}
 
-// 	for _, k := range keys {
-// 		err := rebitcask.Delete(k)
-// 		if err != nil {
-// 			t.Error("Delete operation should work")
-// 		}
-// 	}
+	for _, k := range keys {
+		err := rebitcask.Delete(k)
+		if err != nil {
+			t.Error("Delete operation should work")
+		}
+	}
 
-// 	for _, k := range keys {
-// 		val, status := rebitcask.Get(k)
-// 		if status {
-// 			str := fmt.Sprintf("the key should not exist: %v", val)
-// 			break
-// 			t.Error(str)
-// 		}
-// 	}
-// }
+	for _, k := range keys {
+		val, status := rebitcask.Get(k)
+		if status {
+			str := fmt.Sprintf("the key should not exist: %v", val)
+			t.Error(str)
+		}
+	}
+}
 
-// func TestEmptyGet(t *testing.T) {
-// 	keys, _ := GenerateLowDuplicateRandom(100)
+func TestEmptyGet(t *testing.T) {
+	keys, _ := GenerateLowDuplicateRandom(100)
 
-// 	for _, k := range keys {
-// 		_, status := rebitcask.Get(k)
-// 		if status {
-// 			t.Error("the key should not exist")
-// 		}
-// 	}
-// }
+	for _, k := range keys {
+		_, status := rebitcask.Get(k)
+		if status {
+			t.Error("the key should not exist")
+		}
+	}
+}

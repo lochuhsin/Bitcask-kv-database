bde8d3e09bb092fadbc3b7e3334393f26a80066c
lochuhsin <lochuhsin@gmail.com>
2024-03-08 21:35:28 +0800

    feat(scripts): remove redundant comment

--- scripts/init-network.sh
+++ scripts/init-network.sh
@@ -3,7 +3,6 @@ NETWORK_NAME="cluster"
 
 # Check if the network already exists
 if docker network inspect $NETWORK_NAME &> /dev/null; then
-    # The network already exists
     echo "Network '$NETWORK_NAME' already exists, skipping creation."
 else
     # Create the external network
